0ea64657cbef1c7eb75ebc3092c7588dd43908c9
Rohan Jadhav <rohanjadhav7069@gmail.com>
2023-06-22 16:34:16 +0530

    base setup

---
+++ app/app.js
@@ -0,0 +1,9 @@
+// let button = document.querySelector(".content button");
+// let input = document.querySelector(".content input");
+
+// $(document).ready(function () {
+//   $(".content button").click(function () {
+//     console.log("Pressed");
+//     $("input").passwordGenerator();
+//   });
+// });
